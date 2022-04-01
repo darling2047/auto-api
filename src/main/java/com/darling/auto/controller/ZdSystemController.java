265fa58dab2ff3ad813d66265cde3e5cf769e50a
donglinlin1 <dongll@asiainfo.com>
2022-04-01 19:45:29 +0800

    功能优化

--- src/main/java/com/darling/auto/controller/ZdSystemController.java
+++ src/main/java/com/darling/auto/controller/ZdSystemController.java
@@ -8,6 +8,7 @@ import com.darling.auto.service.ZdSystemService;
 import com.darling.auto.utils.BeanCopierUtils;
 import com.darling.auto.utils.TokenUtil;
 import com.darling.auto.utils.UserUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,7 @@ import java.util.Objects;
  */
 @RestController
 @RequestMapping("system")
+@Slf4j
 public class ZdSystemController {
 
 
@@ -43,6 +45,8 @@ public class ZdSystemController {
     @RequestMapping("/login")
     public ResponResult doLogin(String loginName, String password,
                                 HttpServletRequest request) {
+        String ipAddr = getIpAddr(request);
+        log.info(">>>>>>>>>ipAddr:{},loginName:{},password:{}",ipAddr,loginName,password);
         UserUtils.setRequest(request);
         if (StringUtils.isBlank(loginName) || StringUtils.isBlank(password)) {
             return ResponResult.markError("用户名密码不能为空!");
@@ -62,6 +66,53 @@ public class ZdSystemController {
             return ResponResult.markError("用户信息设置异常:"+e.getMessage());
         }
         return ResponResult.markSuccess(convert);
+    }
+
+    /**
+     * 获取用户真实IP地址，不使用request.getRemoteAddr()的原因是有可能用户使用了代理软件方式避免真实IP地址,
+     * 可是，如果通过了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP值
+     *
+     * @return ip
+     */
+    private String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        try {
+            System.out.println("x-forwarded-for ip: " + ip);
+            if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
+                // 多次反向代理后会有多个ip值，第一个ip才是真实ip
+                if( ip.indexOf(",")!=-1 ){
+                    ip = ip.split(",")[0];
+                }
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("Proxy-Client-IP");
+                System.out.println("Proxy-Client-IP ip: " + ip);
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("WL-Proxy-Client-IP");
+                System.out.println("WL-Proxy-Client-IP ip: " + ip);
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_CLIENT_IP");
+                System.out.println("HTTP_CLIENT_IP ip: " + ip);
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+                System.out.println("HTTP_X_FORWARDED_FOR ip: " + ip);
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("X-Real-IP");
+                System.out.println("X-Real-IP ip: " + ip);
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getRemoteAddr();
+                System.out.println("getRemoteAddr ip: " + ip);
+            }
+            System.out.println("获取客户端ip: " + ip);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ip;
     }
 
 }
